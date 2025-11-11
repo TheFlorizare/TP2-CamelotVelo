85e647f64515f749b265e992064fc362b7a4503f
TheFlorizare <trieuminh.tm2010@gmail.com>
2025-11-11 11:37:28 -0500

    des methodes pour les differentes scenes

--- src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
+++ src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
@@ -2,8 +2,12 @@ package ca.qc.bdeb.sim.tp2camelotvelo;
 
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
+import javafx.geometry.Pos;
 import javafx.scene.Scene;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.control.Button;
 import javafx.scene.layout.Pane;
+import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 import javafx.scene.canvas.GraphicsContext;
 
@@ -11,7 +15,31 @@ import java.io.IOException;
 
 public class MainJavaFX extends Application {
 
+    public static final double WIDTH = 640, HEIGHT = 480;
 
+    private Scene sceneAccueil() {
+        var root = new StackPane();
+        var scene = new Scene(root, WIDTH, HEIGHT);
+        root.setAlignment(Pos.CENTER);
+
+        var btnJouer = new Button("Jouer");
+
+        btnJouer.setOnAction((e) -> {
+            stage;
+        });
+
+        return scene;
+    }
+
+    public Scene sceneNiveau1() {
+        var root = new Pane();
+        var scene = new Scene(root, WIDTH, HEIGHT);
+        var canvas = new Canvas(WIDTH, HEIGHT);
+        root.getChildren().add(canvas);
+        var context = canvas.getGraphicsContext2D();
+
+        return scene;
+    }
     @Override
     public void start(Stage stage) throws IOException {
 
@@ -34,7 +62,7 @@ public class MainJavaFX extends Application {
         timer.start();
 
         var root = new Pane();
-        Scene scene = new Scene(root, 320, 240);
+        Scene scene = new Scene(root, 640, 480);
         stage.setTitle("Hello!");
         stage.setScene(scene);
         stage.show();
