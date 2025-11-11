0a08cc611b0bfe65051e87d8bb44d4451507c6e8
TheFlorizare <trieuminh.tm2010@gmail.com>
2025-11-11 10:39:58 -0500

    hello

--- src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
+++ src/main/java/ca/qc/bdeb/sim/tp2camelotvelo/MainJavaFX.java
@@ -3,6 +3,7 @@ package ca.qc.bdeb.sim.tp2camelotvelo;
 import javafx.application.Application;
 
 import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,6 +12,7 @@ public class MainJavaFX extends Application {
     @Override
     public void start(Stage stage) throws IOException {
 
+        var root = new Pane();
         Scene scene = new Scene(root, 320, 240);
         stage.setTitle("Hello!");
         stage.setScene(scene);
